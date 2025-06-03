a9e5da6fe88d46cac03cc8263cb6668ec59b488e
ijanaxio <ijanzikrim@gmail.com>
2025-06-03 12:35:19 +0700

    Modifikasi agar menerima masukan jumlah nama dari pengguna

--- hallo.cpp
+++ hallo.cpp
@@ -3,19 +3,29 @@
 using namespace std;
 
 int main() {
-    string nama[5];
+    int jumlah;
 
-    nama[0] = "nepnep";
-    nama[1] = "fadli";
-    nama[2] = "rayhan";
-    nama[3] = "kiwil";
-    nama[4] = "Eto";
+    cout << "Masukkan jumlah nama yang akan dimasukkan: ";
+    cin >> jumlah;
+    cin.ignore(); // Membersihkan buffer newline
+
+    // Alokasi array dinamis
+    string* nama = new string[jumlah];
+
+    // Input nama-nama
+    for (int i = 0; i < jumlah; i++) {
+        cout << "Masukkan nama ke-" << i + 1 << ": ";
+        getline(cin, nama[i]);
+    }
 
     // Menampilkan isi array
-    cout << "Daftar Nama:" << endl;
-    for (int i = 0; i < 5; i++) {
+    cout << "\nDaftar Nama:" << endl;
+    for (int i = 0; i < jumlah; i++) {
         cout << i + 1 << ". " << nama[i] << endl;
     }
+
+    // Menghapus alokasi dinamis
+    delete[] nama;
 
     return 0;
 }
