8bd1f6db22492bba108efc9311a7b8179f939fb7
ijanaxio <ijanzikrim@gmail.com>
2025-06-03 12:32:10 +0700

    Menampilkan isi array

--- hallo.cpp
+++ hallo.cpp
@@ -5,12 +5,17 @@ using namespace std;
 int main() {
     string nama[5];
 
-    // Memasukkan data ke array
     nama[0] = "nepnep";
     nama[1] = "fadli";
     nama[2] = "rayhan";
     nama[3] = "kiwil";
     nama[4] = "Eto";
+
+    // Menampilkan isi array
+    cout << "Daftar Nama:" << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << i + 1 << ". " << nama[i] << endl;
+    }
 
     return 0;
 }
