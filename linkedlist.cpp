ac08305dc2d1075c83b2ddcb4efa78b9429c602c
ijanaxio <ijanzikrim@gmail.com>
2025-06-25 13:41:44 +0700

    Menambahkan menu utama untuk mengakses seluruh fitur chat

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -62,6 +62,51 @@ void hitungPesan() {
     }
     cout << "Jumlah pesan: " << jumlah << endl;
 }
+int main() {
+    int pilihan;
+    string isiPesan;
+
+    do {
+        cout << "\n=== MENU RIWAYAT CHAT ===\n";
+        cout << "1. Tambah Pesan\n";
+        cout << "2. Tampilkan Semua Pesan\n";
+        cout << "3. Hapus Pesan Pertama\n";
+        cout << "4. Hapus Semua Pesan\n";
+        cout << "5. Hitung Pesan\n";
+        cout << "0. Keluar\n";
+        cout << "Pilihan: ";
+        cin >> pilihan;
+
+        switch (pilihan) {
+            case 1:
+                cout << "Ketik pesan: ";
+                cin >> ws;
+                getline(cin, isiPesan);
+                tambahPesan(isiPesan);
+                break;
+            case 2:
+                tampilkanPesan();
+                break;
+            case 3:
+                hapusPesanPertama();
+                break;
+            case 4:
+                hapusSemuaPesan();
+                break;
+            case 5:
+                hitungPesan();
+                break;
+            case 0:
+                cout << "Keluar dari program.\n";
+                break;
+             default:
+                cout << "Pilihan tidak valid.\n";
+        }
+    } while (pilihan != 0);
+
+    return 0;
+}
+
 
 
 
